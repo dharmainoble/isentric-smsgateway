8cba33b0edc698185875323b2c6d3d78311a60f2
Arun <smarttrade@texcoms.com>
2026-02-15 11:52:18 +0530

    Changes

--- BulkGateway/src/test/java/com/isentric/bulkgateway/BulkGatewayApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.isentric.bulkgateway;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class BulkGatewayApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
